9a7284b92e5eda39942560c8dc3a25b61cae0378
DiuminPM <75372207+DiuminPM@users.noreply.github.com>
2021-02-16 14:17:39 +0300

    Initial Commit

---
+++ GuessNumberGamesApp/GuessNumberGamesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessNumberGamesAppApp.swift
+//  GuessNumberGamesApp
+//
+//  Created by DiuminPM on 16.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessNumberGamesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
